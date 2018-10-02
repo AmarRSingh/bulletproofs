3aa3788dd8c588622c276e49eaa60fe291e3effb
Cathie Yun <cathieyun@gmail.com>
2018-10-02 12:31:24 -0700

    have shuffle doctest allocate high-level variables

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ pub mod rangeproof_mpc {
 /// use bulletproofs::r1cs::{Assignment, ConstraintSystem, Variable, R1CSError};
 /// use curve25519_dalek::scalar::Scalar;
 ///
-/// // Make a gadget that adds constraints to a ConstraintSystem, such that the 
+/// // Make a gadget that adds constraints to a ConstraintSystem, such that the
 /// // y variables are constrained to be a valid shuffle of the x variables.
 ///
 /// struct KShuffleGadget {}
@@ -73,7 +73,7 @@ pub mod rangeproof_mpc {
 ///         let one = Scalar::one();
 ///         let z = cs.challenge_scalar(b"k-shuffle challenge");
 ///         let neg_z = -z;
-/// 
+///
 ///         if x.len() != y.len() {
 ///             return Err(R1CSError::InvalidR1CSConstruction);
 ///         }
@@ -82,12 +82,12 @@ pub mod rangeproof_mpc {
 ///             cs.add_constraint([(x[0].0, -one), (y[0].0, one)].iter().collect());
 ///             return Ok(());
 ///         }
-/// 
+///
 ///         // Make last x multiplier for i = k-1 and k-2
 ///         let mut mulx_left = x[k - 1].1 + neg_z;
 ///         let mut mulx_right = x[k - 2].1 + neg_z;
 ///         let mut mulx_out = mulx_left * mulx_right;
-/// 
+///
 ///         let mut mulx_out_var_prev = KShuffleGadget::multiplier_helper(
 ///             cs,
 ///             neg_z,
@@ -98,13 +98,13 @@ pub mod rangeproof_mpc {
 ///             x[k - 2].0,
 ///             true,
 ///         )?;
-/// 
+///
 ///         // Make multipliers for x from i == [0, k-3]
 ///         for i in (0..k - 2).rev() {
 ///             mulx_left = mulx_out;
 ///             mulx_right = x[i].1 + neg_z;
 ///             mulx_out = mulx_left * mulx_right;
-/// 
+///
 ///             mulx_out_var_prev = KShuffleGadget::multiplier_helper(
 ///                 cs,
 ///                 neg_z,
@@ -116,12 +116,12 @@ pub mod rangeproof_mpc {
 ///                 false,
 ///             )?;
 ///         }
-/// 
+///
 ///         // Make last y multiplier for i = k-1 and k-2
 ///         let mut muly_left = y[k - 1].1 - z;
 ///         let mut muly_right = y[k - 2].1 - z;
 ///         let mut muly_out = muly_left * muly_right;
-/// 
+///
 ///         let mut muly_out_var_prev = KShuffleGadget::multiplier_helper(
 ///             cs,
 ///             neg_z,
@@ -132,13 +132,13 @@ pub mod rangeproof_mpc {
 ///             y[k - 2].0,
 ///             true,
 ///         )?;
-/// 
+///
 ///         // Make multipliers for y from i == [0, k-3]
 ///         for i in (0..k - 2).rev() {
 ///             muly_left = muly_out;
 ///             muly_right = y[i].1 + neg_z;
 ///             muly_out = muly_left * muly_right;
-/// 
+///
 ///             muly_out_var_prev = KShuffleGadget::multiplier_helper(
 ///                 cs,
 ///                 neg_z,
@@ -150,17 +150,17 @@ pub mod rangeproof_mpc {
 ///                 false,
 ///             )?;
 ///         }
-/// 
+///
 ///         // Check equality between last x mul output and last y mul output
 ///         cs.add_constraint(
 ///             [(muly_out_var_prev, -one), (mulx_out_var_prev, one)]
 ///                 .iter()
 ///                 .collect(),
 ///         );
-/// 
+///
 ///         Ok(())
 ///     }
-/// 
+///
 ///     fn multiplier_helper<CS: ConstraintSystem>(
 ///         cs: &mut CS,
 ///         neg_z: Scalar,
@@ -173,10 +173,10 @@ pub mod rangeproof_mpc {
 ///     ) -> Result<Variable, R1CSError> {
 ///         let one = Scalar::one();
 ///         let var_one = Variable::One();
-/// 
+///
 ///         // Make multiplier gate variables
 ///         let (left_mul_var, right_mul_var, out_mul_var) = cs.assign_multiplier(left, right, out)?;
-/// 
+///
 ///         if is_last_mul {
 ///             // Make last multiplier
 ///             cs.add_constraint(
@@ -193,7 +193,7 @@ pub mod rangeproof_mpc {
 ///                 .iter()
 ///                 .collect(),
 ///         );
-/// 
+///
 ///         Ok(out_mul_var)
 ///     }
 /// }
@@ -232,19 +232,46 @@ pub mod rangeproof_mpc {
 ///     assert!(shuffle_helper(vec![3, 6, 10, 15, 17], vec![3, 6, 10, 15, 3]).is_err());
 /// }
 ///
+/// // This test allocates variables for the high-level variables, to check that high-level
+/// // variable allocation and commitment works.
 /// fn shuffle_helper(input: Vec<u64>, output: Vec<u64>) -> Result<(), R1CSError> {
 ///     // Common
 ///     let pc_gens = PedersenGens::default();
 ///     let bp_gens = BulletproofGens::new(128, 1);
 ///
+///     let k = input.len();
+///     if k != output.len() {
+///         return Err(R1CSError::InvalidR1CSConstruction);
+///     }
+///
 ///     // Prover's scope
 ///     let (proof, commitments) = {
 ///         // Prover makes a `ConstraintSystem` instance representing a shuffle gadget
-///         // v and v_blinding empty because we are only testing low-level variable constraints
-///         let v = vec![];
-///         let v_blinding = vec![];
+///         // Make v vector
+///         let mut v = vec![];
+///         for i in 0..k {
+///             v.push(Scalar::from(input[i]));
+///         }
+///         for i in 0..k {
+///             v.push(Scalar::from(output[i]));
+///         }
+///
+///         // Make v_blinding vector using RNG from transcript
 ///         let mut prover_transcript = Transcript::new(b"ShuffleTest");
-///         let (mut prover_cs, _variables, commitments) = ProverCS::new(
+///         let mut rng = {
+///             let mut builder = prover_transcript.build_rng();
+///
+///             // commit the secret values
+///             for &v_i in &v {
+///                 builder = builder.commit_witness_bytes(b"v_i", v_i.as_bytes());
+///             }
+///
+///             use rand::thread_rng;
+///             builder.finalize(&mut thread_rng())
+///         };
+///         let v_blinding: Vec<Scalar> = (0..2*k).map(|_| Scalar::random(&mut rng)).collect();
+///
+///         let (mut prover_cs, variables, commitments) = prover::ProverCS::new(
 ///             &bp_gens,
 ///             &pc_gens,
 ///             &mut prover_transcript,
@@ -253,15 +280,16 @@ pub mod rangeproof_mpc {
 ///         );
 ///
 ///         // Prover allocates variables and adds constraints to the constraint system
-///         let in_assignments = input
-///             .iter()
-///             .map(|in_i| Assignment::from(in_i.clone()))
+///         let in_pairs = variables[0..k].iter()
+///             .zip(input.iter())
+///             .map(|(var_i, in_i)| (*var_i, Assignment::from(in_i.clone())))
 ///             .collect();
-///         let out_assignments = output
-///             .iter()
-///             .map(|out_i| Assignment::from(out_i.clone()))
+///         let out_pairs = variables[k..2*k].iter()
+///             .zip(output.iter())
+///             .map(|(var_i, out_i)| (*var_i, Assignment::from(out_i.clone())))
 ///             .collect();
-///         shuffle_cs(&mut prover_cs, in_assignments, out_assignments)?;
+///
+///         KShuffleGadget::fill_cs(&mut prover_cs, in_pairs, out_pairs);
 ///         let proof = prover_cs.prove()?;
 ///
 ///         (proof, commitments)
@@ -269,53 +297,18 @@ pub mod rangeproof_mpc {
 ///
 ///     // Verifier makes a `ConstraintSystem` instance representing a shuffle gadget
 ///     let mut verifier_transcript = Transcript::new(b"ShuffleTest");
-///     let (mut verifier_cs, _variables) =
-///         VerifierCS::new(&bp_gens, &pc_gens, &mut verifier_transcript, commitments);
+///     let (mut verifier_cs, variables) =
+///         verifier::VerifierCS::new(&bp_gens, &pc_gens, &mut verifier_transcript, commitments);
 ///
 ///     // Verifier allocates variables and adds constraints to the constraint system
-///     let in_assignments = input.iter().map(|_| Assignment::Missing()).collect();
-///     let out_assignments = output.iter().map(|_| Assignment::Missing()).collect();
-///     assert!(shuffle_cs(&mut verifier_cs, in_assignments, out_assignments,).is_ok());
+///     let in_pairs = variables[0..k].iter().map(|var_i| (*var_i, Assignment::Missing())).collect();
+///     let out_pairs = variables[k..2*k].iter().map(|var_i| (*var_i, Assignment::Missing())).collect();
+///     assert!(KShuffleGadget::fill_cs(&mut verifier_cs, in_pairs, out_pairs).is_ok());
+///
 ///     // Verifier verifies proof
 ///     Ok(verifier_cs.verify(&proof)?)
 /// }
 ///
-/// fn shuffle_cs<CS: ConstraintSystem>(
-///     cs: &mut CS,
-///     input: Vec<Assignment>,
-///     output: Vec<Assignment>,
-/// ) -> Result<(), R1CSError> {
-///     if input.len() != output.len() {
-///         return Err(R1CSError::InvalidR1CSConstruction);
-///     }
-///     let k = input.len();
-///     let mut in_pairs = Vec::with_capacity(k);
-///     let mut out_pairs = Vec::with_capacity(k);
-///
-///     // Allocate pairs of low-level variables and their assignments
-///     for i in 0..k / 2 {
-///         let idx_l = i * 2;
-///         let idx_r = idx_l + 1;
-///         let (in_var_left, in_var_right) = cs.assign_uncommitted(input[idx_l], input[idx_r])?;
-///         in_pairs.push((in_var_left, input[idx_l]));
-///         in_pairs.push((in_var_right, input[idx_r]));
-///
-///         let (out_var_left, out_var_right) =
-///             cs.assign_uncommitted(output[idx_l], output[idx_r])?;
-///         out_pairs.push((out_var_left, output[idx_l]));
-///         out_pairs.push((out_var_right, output[idx_r]));
-///     }
-///     if k % 2 == 1 {
-///         let idx = k - 1;
-///         let (in_var_left, _) = cs.assign_uncommitted(input[idx], Scalar::zero().into())?;
-///         in_pairs.push((in_var_left, input[idx]));
-///         let (out_var_left, _) = cs.assign_uncommitted(output[idx], Scalar::zero().into())?;
-///         out_pairs.push((out_var_left, output[idx]));
-///     }
-///
-///     KShuffleGadget::fill_cs(cs, in_pairs, out_pairs)
-/// }
-/// 
 /// ```
 ///
 
